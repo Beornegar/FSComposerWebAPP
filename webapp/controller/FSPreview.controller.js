e5cf7813c49a93721a86000afc5208b06e064d08
Beornegar <krausdennis@gmx.de>
2020-06-01 21:31:35 +0200

    File Upload and Sending

---
+++ webapp/controller/FSPreview.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+
+  return Controller.extend("lp.fscomposer.controller.FSPreview", {
+    onInit: function () {},
+  });
+});
---
+++ webapp/controller/Master.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast"], function (
+  Controller,
+  MessageToast
+) {
+  "use strict";
+  var input;
+  return Controller.extend("lp.fscomposer.controller.Master", {
+    onInit: function () {},
+  });
+});
