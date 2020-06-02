5d42ce949d0caaec298d90f87c70132697dd9cb1
Beornegar <krausdennis@gmx.de>
2020-06-02 18:36:26 +0200

    flexible column layout and loading of xml data as list

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,28 +1,77 @@
-sap.ui.define(
-  ["sap/ui/core/UIComponent", "sap/ui/Device", "lp/fscomposer/model/models"],
-  function (UIComponent, Device, models) {
-    "use strict";
+sap.ui.define([
+	'sap/ui/core/UIComponent',
+	'sap/ui/model/json/JSONModel',
+	'sap/f/FlexibleColumnLayoutSemanticHelper',
+	'sap/f/library'
+], function(UIComponent, JSONModel, FlexibleColumnLayoutSemanticHelper, fioriLibrary) {
+	'use strict';
 
-    return UIComponent.extend("lp.fscomposer.Component", {
-      metadata: {
-        manifest: "json",
-      },
+	return UIComponent.extend('sap.ui.demo.fiori2.Component', {
 
-      /**
-       * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-       * @public
-       * @override
-       */
-      init: function () {
-        // call the base component's init function
-        UIComponent.prototype.init.apply(this, arguments);
+		metadata: {
+			manifest: 'json'
+		},
 
-        // enable routing
-        this.getRouter().initialize();
+		init: function () {
+			var oModel,
+				oProductsModel,
+				oRouter;
 
-        // set the device model
-        this.setModel(models.createDeviceModel(), "device");
-      },
-    });
-  }
-);
+			UIComponent.prototype.init.apply(this, arguments);
+
+			oModel = new JSONModel();
+			this.setModel(oModel);
+
+			// set products demo model on this sample
+			oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock') + '/products.json');
+			oProductsModel.setSizeLimit(1000);
+			this.setModel(oProductsModel, 'products');
+
+			oRouter = this.getRouter();
+			oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+			oRouter.initialize();
+		},
+
+		getHelper: function () {
+			return this._getFcl().then(function(oFCL) {
+				var oSettings = {
+					defaultTwoColumnLayoutType: fioriLibrary.LayoutType.TwoColumnsMidExpanded,
+					defaultThreeColumnLayoutType: fioriLibrary.LayoutType.ThreeColumnsMidExpanded,
+					initialColumnsCount: 2
+				};
+				return (FlexibleColumnLayoutSemanticHelper.getInstanceFor(oFCL, oSettings));
+			});
+		},
+
+		_onBeforeRouteMatched: function(oEvent) {
+			var oModel = this.getModel(),
+				sLayout = oEvent.getParameters().arguments.layout,
+				oNextUIState;
+
+			// If there is no layout parameter, query for the default level 0 layout (normally OneColumn)
+			if (!sLayout) {
+				this.getHelper().then(function(oHelper) {
+					oNextUIState = oHelper.getNextUIState(0);
+					oModel.setProperty("/layout", oNextUIState.layout);
+				});
+				return;
+			}
+
+			oModel.setProperty("/layout", sLayout);
+		},
+
+		_getFcl: function () {
+			return new Promise(function(resolve, reject) {
+				var oFCL = this.getRootControl().byId('flexibleColumnLayout');
+				if (!oFCL) {
+					this.getRootControl().attachAfterInit(function(oEvent) {
+						resolve(oEvent.getSource().byId('flexibleColumnLayout'));
+					}, this);
+					return;
+				}
+				resolve(oFCL);
+
+			}.bind(this));
+		}
+	});
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,112 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/Controller"],
+  function (JSONModel, Controller) {
+    "use strict";
 
-  return Controller.extend("lp.fscomposer.controller.App", {
-    onInit: function () {},
-  });
-});
+    return Controller.extend("sap.ui.demo.fiori2.controller.App", {
+      onInit: function () {
+        this.oOwnerComponent = this.getOwnerComponent();
+        this.oRouter = this.oOwnerComponent.getRouter();
+        this.oRouter.attachRouteMatched(this.onRouteMatched, this);
+      },
+
+      onRouteMatched: function (oEvent) {
+        var sRouteName = oEvent.getParameter("name"),
+          oArguments = oEvent.getParameter("arguments");
+
+        this._updateUIElements();
+
+        // Save the current route name
+        this.currentRouteName = sRouteName;
+        this.currentProduct = oArguments.product;
+        this.currentSupplier = oArguments.supplier;
+      },
+
+      onStateChanged: function (oEvent) {
+        var bIsNavigationArrow = oEvent.getParameter("isNavigationArrow"),
+          sLayout = oEvent.getParameter("layout");
+
+        this._updateUIElements();
+
+        // Replace the URL with the new layout if a navigation arrow was used
+        if (bIsNavigationArrow) {
+          this.oRouter.navTo(
+            this.currentRouteName,
+            {
+              layout: sLayout,
+              product: this.currentProduct,
+              supplier: this.currentSupplier,
+            },
+            true
+          );
+        }
+      },
+
+      // Update the close/fullscreen buttons visibility
+      _updateUIElements: function () {
+        var oModel = this.oOwnerComponent.getModel(),
+          oUIState;
+        this.oOwnerComponent.getHelper().then(function (oHelper) {
+          oUIState = oHelper.getCurrentUIState();
+          oModel.setData(oUIState);
+        });
+      },
+
+      handleValueChange: function (e) {
+        sap.ui.core.BusyIndicator.show();
+        const file = e.getParameter("files") && e.getParameter("files")[0];
+        const model = this.getView().getModel("puzzelData");
+        const frontendModel = this.getView().getModel("viewData");
+        const saveModel = this.getView().getModel("saveData");
+
+        if (file && window.FileReader) {
+          const reader = new FileReader();
+          const that = this;
+          reader.onload = function (evn) {
+            const strCSV = evn.target.result;
+            console.log(strCSV);
+
+            // create XHR object
+            const xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function () {
+              // 4 means request is finished and response is ready
+              // 200 means ok
+              if (this.readyState == 4 && this.status == 200) {
+                // this refers here to the XHR object
+                // sap.base.Log.info(this.responseText);
+                // console.log(this.responseText);
+                model.setXML(this.responseText);
+                frontendModel.setXML(
+                  model.getObject("/PuzzelGroupModel").outerHTML
+                );
+                saveModel.setXML(model.getObject("/SAP2LEPO").outerHTML);
+                console.log("View-Data-Model");
+                console.log(frontendModel);
+              }
+            };
+
+            // set the XHR request parameters
+            xhttp.open(
+              "POST",
+              "https://localhost:44371/sap2lepo?withFrontendInformation=true",
+              true
+            );
+            xhttp.setRequestHeader("Accept", "*/*");
+            xhttp.setRequestHeader("content-type", "application/xml");
+            // fire the XHR request with data in body
+            xhttp.send(strCSV);
+
+            sap.ui.core.BusyIndicator.hide();
+          };
+          reader.readAsText(file);
+        }
+      },
+
+      onExit: function () {
+        this.oRouter.detachRouteMatched(this.onRouteMatched, this);
+        this.oRouter.detachBeforeRouteMatched(this.onBeforeRouteMatched, this);
+      },
+    });
+  }
+);
