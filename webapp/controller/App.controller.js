651a9a18cf4b549b0d05de336e042f1622d07848
Beornegar <krausdennis@gmx.de>
2020-06-10 19:59:34 +0200

    xml download

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,120 +1,141 @@
-sap.ui.define(
-  ["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/Controller"],
-  function (JSONModel, Controller) {
-    "use strict";
+sap.ui.define(["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/Controller", "sap/ui/core/util/File"], function (JSONModel, Controller, File) {
+  "use strict";
 
-    return Controller.extend("sap.ui.demo.fiori2.controller.App", {
-      onInit: function () {
-        this.oOwnerComponent = this.getOwnerComponent();
-        this.oRouter = this.oOwnerComponent.getRouter();
-        this.oRouter.attachRouteMatched(this.onRouteMatched, this);
+  return Controller.extend("sap.ui.demo.fiori2.controller.App", {
+    onInit: function () {
+      this.oOwnerComponent = this.getOwnerComponent();
+      this.oRouter = this.oOwnerComponent.getRouter();
+      this.oRouter.attachRouteMatched(this.onRouteMatched, this);
 
-        this.getView().getModel("dbList").setXML("<DBList></DBList>");
-        this.getView().getModel("usedList").setXML("<PlAufList></PlAufList>");
-      },
+      this.getView().getModel("dbList").setXML("<root><DBList></DBList></root>");
 
-      onRouteMatched: function (oEvent) {
-        var sRouteName = oEvent.getParameter("name"),
-          oArguments = oEvent.getParameter("arguments");
+      this.getView().getModel("usedList").setXML("<root><PlAufList></PlAufList></root>");
+    },
 
-        this._updateUIElements();
+    onRouteMatched: function (oEvent) {
+      var sRouteName = oEvent.getParameter("name"),
+        oArguments = oEvent.getParameter("arguments");
 
-        // Save the current route name
-        this.currentRouteName = sRouteName;
-        this.currentProduct = oArguments.product;
-        this.currentSupplier = oArguments.supplier;
-      },
+      this._updateUIElements();
 
-      onStateChanged: function (oEvent) {
-        var bIsNavigationArrow = oEvent.getParameter("isNavigationArrow"),
-          sLayout = oEvent.getParameter("layout");
+      // Save the current route name
+      this.currentRouteName = sRouteName;
+      this.currentProduct = oArguments.product;
+      this.currentSupplier = oArguments.supplier;
+    },
 
-        this._updateUIElements();
+    onStateChanged: function (oEvent) {
+      var bIsNavigationArrow = oEvent.getParameter("isNavigationArrow"),
+        sLayout = oEvent.getParameter("layout");
 
-        // Replace the URL with the new layout if a navigation arrow was used
-        if (bIsNavigationArrow) {
-          this.oRouter.navTo(
-            this.currentRouteName,
-            {
-              layout: sLayout,
-              product: this.currentProduct,
-              supplier: this.currentSupplier,
-            },
-            true
-          );
-        }
-      },
+      this._updateUIElements();
 
-      // Update the close/fullscreen buttons visibility
-      _updateUIElements: function () {
-        var oModel = this.oOwnerComponent.getModel(),
-          oUIState;
-        this.oOwnerComponent.getHelper().then(function (oHelper) {
-          oUIState = oHelper.getCurrentUIState();
-          oModel.setData(oUIState);
-        });
-      },
+      // Replace the URL with the new layout if a navigation arrow was used
+      if (bIsNavigationArrow) {
+        this.oRouter.navTo(
+          this.currentRouteName,
+          {
+            layout: sLayout,
+            product: this.currentProduct,
+            supplier: this.currentSupplier,
+          },
+          true
+        );
+      }
+    },
 
-      handleValueChange: function (e) {
-        sap.ui.core.BusyIndicator.show();
-        const file = e.getParameter("files") && e.getParameter("files")[0];
-        const model = this.getView().getModel("puzzelData");
-        const frontendModel = this.getView().getModel("viewData");
-        const saveModel = this.getView().getModel("saveData");
+    // Update the close/fullscreen buttons visibility
+    _updateUIElements: function () {
+      var oModel = this.oOwnerComponent.getModel(),
+        oUIState;
+      this.oOwnerComponent.getHelper().then(function (oHelper) {
+        oUIState = oHelper.getCurrentUIState();
+        oModel.setData(oUIState);
+      });
+    },
 
-        if (file && window.FileReader) {
-          const reader = new FileReader();
-          const that = this;
-          reader.onload = function (evn) {
-            const strCSV = evn.target.result;
-            console.log(strCSV);
+    handleValueChange: function (e) {
+      sap.ui.core.BusyIndicator.show();
+      const file = e.getParameter("files") && e.getParameter("files")[0];
+      const model = this.getView().getModel("puzzelData");
+      const frontendModel = this.getView().getModel("viewData");
+      const saveModel = this.getView().getModel("saveData");
 
-            // create XHR object
-            const xhttp = new XMLHttpRequest();
+      console.log(file);
+      this.getView().getModel().setProperty("/filename", file.name);
 
-            xhttp.onreadystatechange = function () {
-              // 4 means request is finished and response is ready
-              // 200 means ok
-              if (this.readyState == 4 && this.status == 200) {
-                // this refers here to the XHR object
-                // sap.base.Log.info(this.responseText);
-                // console.log(this.responseText);
-                model.setXML(this.responseText);
-                frontendModel.setXML(
-                  model.getObject("/PuzzelGroupModel").outerHTML
-                );
-                saveModel.setXML(model.getObject("/SAP2LEPO").outerHTML);
-                console.log("View-Data-Model");
-                console.log(frontendModel);
-              }
-            };
+      if (file && window.FileReader) {
+        const reader = new FileReader();
+        const that = this;
+        reader.onload = function (evn) {
+          const strCSV = evn.target.result;
 
-            // set the XHR request parameters
-            xhttp.open(
-              "POST",
-              "https://localhost:44371/sap2lepo?withFrontendInformation=true",
-              true
-            );
-            xhttp.setRequestHeader("Accept", "*/*");
-            xhttp.setRequestHeader("content-type", "application/xml");
-            // fire the XHR request with data in body
-            xhttp.send(strCSV);
+          // create XHR object
+          const xhttp = new XMLHttpRequest();
 
-            sap.ui.core.BusyIndicator.hide();
+          xhttp.onreadystatechange = function () {
+            // 4 means request is finished and response is ready
+            // 200 means ok
+            if (this.readyState == 4 && this.status == 200) {
+              // this refers here to the XHR object
+              // sap.base.Log.info(this.responseText);
+              // console.log(this.responseText);
+              model.setXML(this.responseText);
+              frontendModel.setXML(model.getObject("/PuzzelGroupModel").outerHTML);
+              saveModel.setXML(model.getObject("/SAP2LEPO").outerHTML);
+              console.log("View-Data-Model");
+              console.log(frontendModel);
+            }
           };
-          reader.readAsText(file);
+
+          // set the XHR request parameters
+          xhttp.open("POST", "https://localhost:44371/sap2lepo?withFrontendInformation=true", true);
+          xhttp.setRequestHeader("Accept", "*/*");
+          xhttp.setRequestHeader("content-type", "application/xml");
+          // fire the XHR request with data in body
+          xhttp.send(strCSV);
+
+          sap.ui.core.BusyIndicator.hide();
+        };
+        reader.readAsText(file);
+      }
+    },
+
+    onExit: function () {
+      this.oRouter.detachRouteMatched(this.onRouteMatched, this);
+      this.oRouter.detachBeforeRouteMatched(this.onBeforeRouteMatched, this);
+    },
+
+    onSaveBtnPress: function () {
+      var file = this.getView().getModel().getProperty("/filename");
+      const fileparts = file.split(".");
+      var filename = "default";
+      var fileExtension = "xml";
+      if (fileparts.length === 2) {
+        filename = fileparts[0] + "_OUT";
+        fileExtension = fileparts[1];
+      }
+
+      const saveDataModel = this.getView().getModel("saveData");
+
+      const dbListModel = this.getView().getModel("dbList");
+      const usedListModel = this.getView().getModel("usedList");
+
+      const dbListElement = dbListModel.getObject("/DBList").cloneNode(true);
+      const usedListElement = usedListModel.getObject("/PlAufList").cloneNode(true);
+
+      const outputElement = saveDataModel.getObject("/OutPut");
+
+      Array.from(outputElement.children).forEach(function (element) {
+        if (element.nodeName === "DBList" || element.nodeName === "PlAufList") {
+          outputElement.removeChild(element);
         }
-      },
+      });
 
-      onExit: function () {
-        this.oRouter.detachRouteMatched(this.onRouteMatched, this);
-        this.oRouter.detachBeforeRouteMatched(this.onBeforeRouteMatched, this);
-      },
+      outputElement.appendChild(dbListElement);
+      outputElement.appendChild(usedListElement);
 
-      onSaveBtnPress: function () {
-        const model = this.getView().getModel("viewData");
-        const model2 = this.getView().getModel("saveData");
-      },
-    });
-  }
-);
+      File.save(saveDataModel.getXML(), filename, fileExtension);
+    },
+  });
+});
--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,59 +1,76 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/demo/fiori2/model/formatter"],
-  function (Controller, formatter) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/demo/fiori2/model/formatter"], function (Controller, formatter) {
+  "use strict";
 
-    return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
-      formatter: formatter,
+  return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
+    formatter: formatter,
 
-      onInit: function () {
-        this.oView = this.getView();
-        this._bDescendingSort = false;
-        this.oProductsTable = this.oView.byId("productsTable");
-        this.oRouter = this.getOwnerComponent().getRouter();
-      },
+    onInit: function () {
+      this.oView = this.getView();
+      this._bDescendingSort = false;
+      this.oProductsTable = this.oView.byId("productsTable");
+      this.oRouter = this.getOwnerComponent().getRouter();
+    },
 
-      onSelectionChange: function (oEvent) {
-        var dbListModel = this.getView().getModel("dbList");
-        var usedListModel = this.getView().getModel("usedList");
+    onSelectionChange: function (oEvent) {
+      const dbListModel = this.getView().getModel("dbList");
+      const usedListModel = this.getView().getModel("usedList");
+      const viewDataModel = this.getView().getModel("viewData");
 
-        const selected = oEvent.getParameters().selected;
-        const selectedItem = oEvent
-          .getParameters()
-          .listItem.getBindingContext("viewData")
-          .getObject();
+      const selected = oEvent.getParameters().selected;
 
-        if (selected) {
-          dbListModel
-            .getProperty("/DBList")
-            .setData(selectedItem.getObject("/DBList"));
+      const dbListItem = viewDataModel.getObject(oEvent.getParameters().listItem.getBindingContext("viewData").sPath + "/DBList");
+      const plaufListItem = viewDataModel.getObject(oEvent.getParameters().listItem.getBindingContext("viewData").sPath + "/PlAufList");
 
-          usedListModel
-            .getProperty("/PlAufList")
-            .setData(selectedItem.getObject("/PlAufList"));
-        } else {
-        }
-      },
+      if (selected) {
+        const dbList = dbListModel.getObject("/DBList");
+        Array.from(dbListItem.children).forEach(function (element) {
+          const clone = element.cloneNode(true);
+          dbList.appendChild(clone);
+        });
 
-      onListItemPress: function (oEvent) {
-        var productPath = oEvent
-            .getSource()
-            .getBindingContext("viewData")
-            .getPath(),
-          product = productPath.split("/").slice(-1).pop(),
-          oNextUIState;
-        this.getOwnerComponent()
-          .getHelper()
-          .then(
-            function (oHelper) {
-              oNextUIState = oHelper.getNextUIState(1);
-              this.oRouter.navTo("detail", {
-                layout: oNextUIState.layout,
-                product: product,
-              });
-            }.bind(this)
-          );
-      },
-    });
-  }
-);
+        const usedList = usedListModel.getObject("/PlAufList");
+        Array.from(plaufListItem.children).forEach(function (element) {
+          const clone = element.cloneNode(true);
+          usedList.appendChild(clone);
+        });
+      } else {
+        const dbList = dbListModel.getObject("/DBList");
+        Array.from(dbListItem.children).forEach(function (elementToRemove) {
+          //Search through children and look if children has attribute "ID" with same value
+          Array.from(dbList.children).forEach(function (elementInList) {
+            if (elementToRemove.getAttribute("ID") === elementInList.getAttribute("ID")) {
+              dbList.removeChild(elementInList);
+            }
+          });
+        });
+
+        const usedList = usedListModel.getObject("/PlAufList");
+        Array.from(plaufListItem.children).forEach(function (elementToRemove) {
+          //Search through children and look if children has attribute "ID" with same value
+          Array.from(usedList.children).forEach(function (elementInList) {
+            if (elementToRemove.getAttribute("ID") === elementInList.getAttribute("ID")) {
+              usedList.removeChild(elementInList);
+            }
+          });
+        });
+      }
+    },
+
+    onListItemPress: function (oEvent) {
+      var productPath = oEvent.getSource().getBindingContext("viewData").getPath(),
+        product = productPath.split("/").slice(-1).pop(),
+        oNextUIState;
+      this.getOwnerComponent()
+        .getHelper()
+        .then(
+          function (oHelper) {
+            oNextUIState = oHelper.getNextUIState(1);
+            this.oRouter.navTo("detail", {
+              layout: oNextUIState.layout,
+              product: product,
+            });
+          }.bind(this)
+        );
+    },
+  });
+});
